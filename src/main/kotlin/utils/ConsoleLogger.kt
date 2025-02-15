dbabe9741c95b2f36a30fbdc2ec2c4b47437f600
NoursInDev <nours44.mc@gmail.com>
2025-02-15 23:55:48 +0100

    Utils - log form

---
+++ src/main/kotlin/utils/ConsoleLogger.kt
@@ -0,0 +1,13 @@
+package utils
+
+fun log(message: String) {
+    println("LOG: $message")
+}
+
+fun logError(message: String) {
+    println("ERROR: $message")
+}
+
+fun logWarning(message: String) {
+    println("WARNING: $message")
+}
