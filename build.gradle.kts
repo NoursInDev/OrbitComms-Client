996e5e3c11cabbd0e6c73fe03c88fed01cf85593
NoursInDev <nours44.mc@gmail.com>
2025-02-16 12:03:40 +0100

    Project - Gradle modification for tesseract

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,13 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+    implementation(files("libs/lept4j-1.21.0.jar"))
+    implementation(files("libs/tess4j-5.15.0.jar"))
+    implementation("net.java.dev.jna:jna:5.9.0")
+    implementation("org.slf4j:slf4j-api:2.0.7")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("com.github.jai-imageio:jai-imageio-core:1.4.0")
+    implementation("commons-io:commons-io:2.18.0")
 }
 
 compose.desktop {
